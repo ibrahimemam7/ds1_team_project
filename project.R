b13f2d4e33fc114483a1cec3ddb0a8814d9e88ba
ibrahimemam7 <1ibrahimemam@gmail.com>
2024-08-07 03:35:59 -0400

    began experimenting with ESS linear model

--- project.R
+++ project.R
@@ -4,6 +4,38 @@ library(tidyverse)
 library(MASS)
 library(mice)
 
+#######################
+## Declare Functions ##
+#######################
+
+#' having functions at the top of the file is important because they will be
+#' available to use in the environment if the file is sourced (runs from top
+#' to bottom)
+
+#' function to determine the max number of degrees of freedom in the predictor
+#' for a given response variable
+max.predictors <- function(response_var, dataframe) {
+  if(is.numeric(dataframe[[response_var]])) {
+    
+    # the variable is numeric
+    tmp <- dataframe %>%
+      summarise(n_observations = n())
+    
+    return(floor(tmp$n_observations/15))
+      
+  } else {
+    
+    # the variable is logical
+    tmp <- dataframe %>% 
+      summarise(
+        true_count = sum(get(response_var)),
+        false_count = sum(!get(response_var))
+      )
+    
+    return(floor(min(tmp$true_count, tmp$false_count) / 15))
+  
+  }
+}
 
 ###############
 ## Data Prep ##
@@ -92,3 +124,69 @@ d_complete$AIS[d_complete$AIS < 0] <- 0
 #' it seems mice() converted BSS column from logical to numerical, so it should be
 #' converted back to logical
 d_complete$BSS <- as.logical(d_complete$BSS)
+
+##############################
+## Sleep Disturbance Models ##
+##############################
+
+# determine the max number of predictors for a model with ESS as a response variable
+max.predictors("BSS", d_complete)
+
+# experiment with stepwise models
+ess.full.mod <- lm(ESS~., data = d_complete)
+ess.null.mod <- lm(ESS~1, data = d_complete)
+
+ess.step.back <- stepAIC(ess.full.mod, trace = F)
+summary(ess.step.back)
+
+ess.step.forw <- stepAIC(ess.null.mod, direction = "forward", trace = F, scope = list(upper=ess.full.mod, lower=ess.null.mod))
+summary(ess.step.forw)
+
+# create a liner model based on clinical literature
+ess.mod <- lm(ESS ~ Gender + Time.from.transplant + BMI + Depression, data = d_complete)
+summary(ess.mod)
+
+# create a simpler model without time from transplant and BMI, then compare
+ess.mod1 <- lm(ESS ~ Gender + Depression, data = d_complete)
+summary(ess.mod1)
+
+AIC(ess.mod, ess.mod1)
+
+# the simpler model (model 1) is better
+
+# can try adding a predictor from the stepwise function and see if the model is improved
+ess.mod2 <- lm(ESS ~ Gender + Depression + Rejection.graft.dysfunction, data = d_complete)
+summary(ess.mod2)
+
+AIC(ess.mod1, ess.mod2)
+
+# the more complex model (model 2) is better
+
+#' can try adding another predictor since there are still two degrees of freedom
+#' in the predictors that can be used
+ess.mod3 <- lm(ESS ~ Gender + Depression + Rejection.graft.dysfunction + Renal.Failure, data = d_complete)
+summary(ess.mod3)
+
+AIC(ess.mod2, ess.mod3)
+
+# the simpler model (model 2) is better than model 3. Can try adding a different predictor
+ess.mod4 <- lm(ESS ~ Gender + Depression + Rejection.graft.dysfunction + Recurrence.of.disease, data = d_complete)
+summary(ess.mod4)
+
+AIC(ess.mod2, ess.mod4)
+
+# the simpler model (model 2) is better than model 4. Can try adding a different predictor
+ess.mod5 <- lm(ESS ~ Gender + Depression + Rejection.graft.dysfunction + Liver.Diagnosis, data = d_complete)
+summary(ess.mod5)
+
+AIC(ess.mod2, ess.mod5)
+
+# the simpler model (model 2) is better than model 5
+
+# can try a more complex model
+ess.mod6 <- lm(ESS ~ Gender + Depression + Rejection.graft.dysfunction + Recurrence.of.disease + Liver.Diagnosis, data = d_complete)
+summary(ess.mod6)
+
+AIC(ess.mod2, ess.mod6) 
+
+# simpler model (model 2) is still better
